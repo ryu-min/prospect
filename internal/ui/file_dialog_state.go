0b030b79bcd8c35763fc257c37db083f2928a832
ryu-min <SergeyR6@yandex.ru>
2026-01-07 17:01:02 +0300

    Share file dialog state between open and save dialogs

--- internal/ui/file_dialog_state.go
+++ internal/ui/file_dialog_state.go
@@ -9,8 +9,7 @@ import (
 )
 
 type fileDialogState struct {
-	lastOpenDirPath   string
-	lastSaveDirPath   string
+	lastDirPath       string
 	lastSchemaDirPath string
 	dialogSize        fyne.Size
 }
@@ -22,15 +21,14 @@ func getFileDialogState() *fileDialogState {
 		wd, _ := os.Getwd()
 		globalDialogState = &fileDialogState{
 			dialogSize:        fyne.NewSize(800, 600),
-			lastOpenDirPath:   wd,
-			lastSaveDirPath:   wd,
+			lastDirPath:       wd,
 			lastSchemaDirPath: wd,
 		}
 	}
 	return globalDialogState
 }
 
-func (fds *fileDialogState) setLastOpenDir(uri fyne.URI) {
+func (fds *fileDialogState) setLastDir(uri fyne.URI) {
 	if uri == nil {
 		return
 	}
@@ -39,21 +37,16 @@ func (fds *fileDialogState) setLastOpenDir(uri fyne.URI) {
 	dir := filepath.Dir(path)
 
 	if info, err := os.Stat(dir); err == nil && info.IsDir() {
-		fds.lastOpenDirPath = dir
+		fds.lastDirPath = dir
 	}
 }
 
+func (fds *fileDialogState) setLastOpenDir(uri fyne.URI) {
+	fds.setLastDir(uri)
+}
+
 func (fds *fileDialogState) setLastSaveDir(uri fyne.URI) {
-	if uri == nil {
-		return
-	}
-
-	path := uri.Path()
-	dir := filepath.Dir(path)
-
-	if info, err := os.Stat(dir); err == nil && info.IsDir() {
-		fds.lastSaveDirPath = dir
-	}
+	fds.setLastDir(uri)
 }
 
 func (fds *fileDialogState) setLastSchemaDir(uri fyne.URI) {
@@ -69,8 +62,8 @@ func (fds *fileDialogState) setLastSchemaDir(uri fyne.URI) {
 	}
 }
 
-func (fds *fileDialogState) getLastOpenDir() fyne.ListableURI {
-	dirPath := fds.lastOpenDirPath
+func (fds *fileDialogState) getLastDir() fyne.ListableURI {
+	dirPath := fds.lastDirPath
 	if dirPath == "" {
 		wd, _ := os.Getwd()
 		dirPath = wd
@@ -93,28 +86,12 @@ func (fds *fileDialogState) getLastOpenDir() fyne.ListableURI {
 	return nil
 }
 
+func (fds *fileDialogState) getLastOpenDir() fyne.ListableURI {
+	return fds.getLastDir()
+}
+
 func (fds *fileDialogState) getLastSaveDir() fyne.ListableURI {
-	dirPath := fds.lastSaveDirPath
-	if dirPath == "" {
-		wd, _ := os.Getwd()
-		dirPath = wd
-	}
-
-	if info, err := os.Stat(dirPath); err != nil || !info.IsDir() {
-		wd, _ := os.Getwd()
-		dirPath = wd
-	}
-
-	uri := storage.NewFileURI(dirPath)
-	if listableURI, err := storage.ListerForURI(uri); err == nil {
-		return listableURI
-	}
-
-	wd, _ := os.Getwd()
-	if listableURI, err := storage.ListerForURI(storage.NewFileURI(wd)); err == nil {
-		return listableURI
-	}
-	return nil
+	return fds.getLastDir()
 }
 
 func (fds *fileDialogState) getLastSchemaDir() fyne.ListableURI {
